8ddf557e480dcbfc695417bdbabcb0a264e7a16b
kamrulkoche <kamrulkoche@gmail.com>
2024-09-15 23:07:29 +0600

    add marquee

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,14 @@ import Header from "../../shared/Header/Header";
 import Navbar from "../../shared/Navbar/Navbar";
 import LeftSideNav from "../../shared/LeftSideNav/LeftSideNav";
 import RightSideNav from "../../shared/RightSideNav/RightSideNav";
+import BreakingNews from "./BreakingNews";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <BreakingNews/>
       <Navbar />
-      <h2 className="text-3xl font-poppins">This is Home page</h2>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
         <div>
           <LeftSideNav />
