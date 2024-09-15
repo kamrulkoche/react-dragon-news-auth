91777bb60813d18796cbef07b82d728f7bcaba3b
kamrulkoche <kamrulkoche@gmail.com>
2024-09-15 22:14:43 +0600

    add date_time

---
+++ src/shared/Header/Header.jsx
@@ -0,0 +1,14 @@
+import moment from 'moment';
+import logo from '../../assets/logo.png'
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logo} alt="" />
+      <p className='text-lg '>Journalism Without Fear or Favour</p>
+      <p className='text-xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
+    </div>
+  );
+};
+
+export default Header;
